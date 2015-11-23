10660ef2edfed392df556e788634e3e8974efe78
cdeldon <carlo.deldon@gmail.com>
2015-11-23 14:20:59 +0100

    added flag to save the animation as an mp4

--- code/output/animate_new.py
+++ code/output/animate_new.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
+saveAnim = False;
+
 if len(sys.argv)<=1:
     data = np.loadtxt("cars_test.dat")
 else :
     data = np.loadtxt(sys.argv[1])
+    if len(sys.argv) ==3:
+        saveAnim = (sys.argv[2] == "save")
 
 
 n = (len(data[0])-1)//2
@@ -51,12 +55,16 @@ def animate(i):
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=len(tt), interval=1, blit=False)
-
+                
+if saveAnim:               
+    anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+else:                              
+    plt.show()
+                            
 # save the animation as an mp4.  This requires ffmpeg or mencoder to be
 # installed.  The extra_args ensure that the x264 codec is used, so that
 # the video can be embedded in html5.  You may need to adjust this for
 # your system: for more information, see
 # http://matplotlib.sourceforge.net/api/animation_api.html
-#anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
-plt.show()
+
